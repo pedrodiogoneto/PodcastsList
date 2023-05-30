ad4f6a8cc37c4525c66c71809b399ab24e6b6494
pedrodiogoneto <pedritoneto@gmail.com>
2023-05-30 11:22:35 +0200

    feat: update .gitignore and add .env

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Homepage from './pages/homepage';
 
 function App() {
 	return (
-		<div className="w-full h-full">
+		<div className="w-full h-full p-8">
 			<Switch>
 				<Route exact path="/">
 					<Homepage />
