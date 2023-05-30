24adaa5b2d107530494ea254804c7366e9a0403c
pedrodiogoneto <pedritoneto@gmail.com>
2023-05-30 11:23:33 +0200

    feat: Add loader component

---
+++ src/components/loader/index.tsx
@@ -0,0 +1,18 @@
+import './index.css';
+
+const Loader = () => {
+	return (
+		<div className="lds-roller">
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	);
+};
+
+export default Loader;
