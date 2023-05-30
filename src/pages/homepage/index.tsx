9da86cb682c387f1150d9000187f497a77ac341d
pedrodiogoneto <pedritoneto@gmail.com>
2023-05-30 11:37:55 +0200

    feat: Add initial search funtion

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -4,6 +4,7 @@ import { getAllPodcasts } from '../../services/podcasts';
 
 const Homepage = () => {
 	const [podcasts, setPodcasts] = useState<any>();
+	const [search, setSearch] = useState<string>();
 
 	const handleData = (data: any) => {
 		const dic: any = {};
@@ -36,6 +37,19 @@ const Homepage = () => {
 		}
 	};
 
+	const searchBy = (data: any) => {
+		if (!search) return true;
+		if (
+			search?.length &&
+			(data?.name?.toLowerCase()?.includes(search?.toLowerCase()) ||
+				data?.author.toLowerCase()?.includes(search?.toLowerCase()))
+		) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
 	useEffect(() => {
 		const isLocalStoragePopulated = localStorage?.getItem('podcasts');
 
@@ -51,9 +65,10 @@ const Homepage = () => {
 			<Navbar isLoading={!podcasts} />
 			<div className="flex items-center justify-end mt-4">
 				<span className="badge mr-4 bg-blue-500 rounded-md border-0 text-bold text-lg py-3 px-2">
-					100
+					{podcasts && Object?.keys(podcasts)?.length}
 				</span>
 				<input
+					onChange={(e) => setSearch(e.target?.value)}
 					type="text"
 					placeholder="Filter podcasts..."
 					className="border rounded-md px-4 py-3"
@@ -61,15 +76,17 @@ const Homepage = () => {
 			</div>
 			<div className="mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
 				{podcasts &&
-					Object.values(podcasts)?.map((podcast: any) => {
-						return (
-							<div key={podcast.id} /*  onClick={() => history.push('/')} */>
-								<img src={podcast?.image} />
-								<h3>{podcast?.name}</h3>
-								<h4>{podcast?.author}</h4>
-							</div>
-						);
-					})}
+					Object.values(podcasts)
+						?.filter(searchBy)
+						?.map((podcast: any) => {
+							return (
+								<div key={podcast.id} /*  onClick={() => history.push('/')} */>
+									<img src={podcast?.image} />
+									<h3>{podcast?.name}</h3>
+									<h4>{podcast?.author}</h4>
+								</div>
+							);
+						})}
 			</div>
 		</div>
 	);
