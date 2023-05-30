a3828b7572474786a3f3a724a11a209bd15018ac
pedrodiogoneto <pedritoneto@gmail.com>
2023-05-30 09:32:01 +0200

    feat: Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Switch, Route } from 'react-router-dom';
+import './index.css';
+import Homepage from './pages/homepage';
+
+function App() {
+	return (
+		<div className="w-full h-full">
+			<Switch>
+				<Route exact path="/">
+					<Homepage />
+				</Route>
+			</Switch>
+		</div>
+	);
+}
+
+export default App;
