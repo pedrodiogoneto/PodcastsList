cba69db798a49ec8b140890dc3989fa82a59d22f
pedrodiogoneto <pedritoneto@gmail.com>
2023-05-30 11:24:43 +0200

    feat: Add initial homepage with fetch and grid render

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,5 +1,78 @@
+import { useEffect, useState } from 'react';
+import Navbar from '../../components/navbar';
+import { getAllPodcasts } from '../../services/podcasts';
+
 const Homepage = () => {
-	return <div>Homepage</div>;
+	const [podcasts, setPodcasts] = useState<any>();
+
+	const handleData = (data: any) => {
+		const dic: any = {};
+
+		data?.map((item: any) => {
+			dic[item?.id?.attributes?.['im:id']] = {
+				image: item?.['im:image']?.[item?.['im:image']?.length - 1]?.label,
+				name: item?.['im:name']?.label,
+				author: item?.['im:artist']?.label,
+				id: item?.id?.attributes?.['im:id'],
+				summary: item?.summary?.label,
+			};
+		});
+
+		return dic;
+	};
+
+	const getPodcasts = async () => {
+		try {
+			const res = await getAllPodcasts();
+
+			if (res?.status === 200) {
+				const parsedData = handleData(res?.data?.feed?.entry);
+
+				await localStorage.setItem('podcasts', JSON.stringify(parsedData));
+				setPodcasts(parsedData);
+			}
+		} catch (error) {
+			console.log('ERR>>>', error);
+		}
+	};
+
+	useEffect(() => {
+		const isLocalStoragePopulated = localStorage?.getItem('podcasts');
+
+		if (isLocalStoragePopulated) {
+			setPodcasts(JSON.parse(isLocalStoragePopulated));
+		} else {
+			getPodcasts();
+		}
+	}, []);
+
+	return (
+		<div>
+			<Navbar isLoading={!podcasts} />
+			<div className="flex items-center justify-end mt-4">
+				<span className="badge mr-4 bg-blue-500 rounded-md border-0 text-bold text-lg py-3 px-2">
+					100
+				</span>
+				<input
+					type="text"
+					placeholder="Filter podcasts..."
+					className="border rounded-md px-4 py-3"
+				/>
+			</div>
+			<div className="mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+				{podcasts &&
+					Object.values(podcasts)?.map((podcast: any) => {
+						return (
+							<div key={podcast.id} /*  onClick={() => history.push('/')} */>
+								<img src={podcast?.image} />
+								<h3>{podcast?.name}</h3>
+								<h4>{podcast?.author}</h4>
+							</div>
+						);
+					})}
+			</div>
+		</div>
+	);
 };
 
 export default Homepage;
